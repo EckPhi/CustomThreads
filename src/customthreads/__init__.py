84597fb2e31b2f52cc31d1d2d6f36051d2e11da1
Philip <44484441+EckPhi@users.noreply.github.com>
2026-03-29 12:47:33 +0200

    Refactors thread generation into a Python package

---
+++ src/customthreads/__init__.py
@@ -0,0 +1,14 @@
+"""3D-Printed Metric Threads Generator - Core package."""
+
+from .models import Thread, ThreadProfile, MetricThreadGenerator
+from .generator import generate_pitch_list, format_number, generate_xml
+
+__version__ = "1.0.0"
+__all__ = [
+    "Thread",
+    "ThreadProfile", 
+    "MetricThreadGenerator",
+    "generate_pitch_list",
+    "format_number",
+    "generate_xml",
+]
